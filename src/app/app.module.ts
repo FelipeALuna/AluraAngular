b8c9c7b44c2f7d53eb8bf8cf80669fcfda72fc4e
FelipeALuna <felipeluna_2000@hotmail.com>
2021-09-12 02:36:15 -0300

    Preparando sistema para rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app.routing.model';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -18,7 +19,8 @@ registerLocaleData(localePt,'pt');
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [
     {provide: LOCALE_ID, useValue:"pt"},
